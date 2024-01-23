d50eab987df9c55b9e59a1bbf295c8ba2e7e80f2
Fredrik Berglund <fredrik@diam.se>
2024-01-23 12:45:59 +0100

    Updates

--- sinus-minishop/js/script.js
+++ sinus-minishop/js/script.js
@@ -1,1 +1,10 @@
 // Code here!
+
+const productHeader = document.querySelector(".art-1 h3");
+productHeader.innerText = "Potato";
+
+const nav = document.querySelector("nav");
+nav.children[0].innerText = "Start";
+
+const contact = document.querySelector("footer article:first-child a:last-child");
+contact.innerText = "Mail Us";
